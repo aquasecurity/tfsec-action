2f9da247f6ef013b5f2ae5b3c59dc1a4865533ee
Jan-Otto Kröpke <github@jkroepke.de>
2023-02-24 16:29:52 +0100

    Avoid calling API

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,46 +1,38 @@
 #!/usr/bin/env bash
 
-set -xe
+set -ex
 
-if [ -z "${INPUT_GITHUB_TOKEN}" ] ; then
-  echo "::notice title=GitHub API token::Consider setting a GITHUB_TOKEN to prevent GitHub api rate limits"
-fi
-
-TFSEC_VERSION=""
 if [ "$INPUT_VERSION" != "latest" ] && [ -n "$INPUT_VERSION" ]; then
-  TFSEC_VERSION="tags/${INPUT_VERSION}"
+  TFSEC_VERSION="download/${INPUT_VERSION}"
 else
-  TFSEC_VERSION="latest"
+  TFSEC_VERSION="latest/download"
 fi
-
-function get_release_assets() {
-  repo="$1"
-  version="$2"
-  args=(
-    -sSL
-    --header "Accept: application/vnd.github+json"
-  )
-  [ -n "${INPUT_GITHUB_TOKEN}" ] && args+=(--header "Authorization: Bearer ${INPUT_GITHUB_TOKEN}")
-
-  if ! curl --fail-with-body -sS "${args[@]}" "https://api.github.com/repos/${repo}/releases/${version}"; then
-    echo "::error title=GitHub API request failure::The request to the GitHub API was likely rate-limited. Set a GITHUB_TOKEN to prevent this"
-    exit 1
-  else
-    curl "${args[@]}" "https://api.github.com/repos/${repo}/releases/${version}" | jq '.assets[] | { name: .name, download_url: .browser_download_url }'
-  fi
-}
 
 function install_release() {
   repo="$1"
   version="$2"
   binary="$3-linux-amd64"
   checksum="$4"
-  release_assets="$(get_release_assets "${repo}" "${version}")"
 
-  curl -sLo "${binary}" "$(echo "${release_assets}" | jq -r ". | select(.name == \"${binary}\") | .download_url")"
-  curl -sLo "$3-checksums.txt" "$(echo "${release_assets}" | jq -r ". | select(.name | contains(\"$checksum\")) | .download_url")"
+  if ! curl -sSfL "https://github.com/aquasecurity/tfsec/releases/${TFSEC_VERSION}/${checksum}" -o "${checksum}"; then
+    echo "Failed to download https://github.com/aquasecurity/tfsec/releases/${TFSEC_VERSION}/${checksum}"
+    exit 1
+  fi
 
-  grep "${binary}" "$3-checksums.txt" | sha256sum -c -
+  if ! curl -sSfL "https://github.com/aquasecurity/tfsec/releases/${TFSEC_VERSION}/${binary}" -o "${binary}"; then
+    echo "Failed to download https://github.com/aquasecurity/tfsec/releases/${TFSEC_VERSION}/${binary}"
+    exit 1
+  fi
+
+  if ! sha256sum --quiet --ignore-missing -w -c "${checksum}"; then
+    echo "Checksum mismatch!"
+    printf "Expected: "
+    grep "${binary}" "${checksum}"
+    printf "Actual:   "
+    sha256sum "${binary}"
+    exit 1
+  fi
+
   install "${binary}" "/usr/local/bin/${3}"
 }
 
@@ -54,7 +46,7 @@ if [ -n "${INPUT_ADDITIONAL_ARGS}" ]; then
   TFSEC_ARGS_OPTION="${INPUT_ADDITIONAL_ARGS}"
 fi
 
-if [ -n "${INPUT_SOFT_FAIL}" ]; then 
+if [ -n "${INPUT_SOFT_FAIL}" ]; then
   SOFT_FAIL="--soft-fail"
 fi
 
